67e518ee749af8dc0d3e9f5028e9cec25b13103e
realVato <44781061+realVato@users.noreply.github.com>
2019-05-23 09:46:31 +0300

    mQuery for JS

--- js/script.js
+++ js/script.js
@@ -2,15 +2,27 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const main = document.querySelector('.main');
 
-left.addEventListener('mouseenter', () => {
-    main.classList.add('hover-left');
-});
-left.addEventListener('mouseleave', () => {
-    main.classList.remove('hover-left');
-});
-right.addEventListener('mouseenter', () => {
-    main.classList.add('hover-right');
-});
-right.addEventListener('mouseleave', () => {
-    main.classList.remove('hover-right');
-});
+function isWindowSmall()
+{
+  if (window.innerWidth > 800) {
+    left.addEventListener('mouseenter', () => {
+        main.classList.add('hover-left');
+    });
+    left.addEventListener('mouseleave', () => {
+        main.classList.remove('hover-left');
+    });
+    right.addEventListener('mouseenter', () => {
+        main.classList.add('hover-right');
+    });
+    right.addEventListener('mouseleave', () => {
+        main.classList.remove('hover-right');
+    });
+    
+  };
+}
+
+isWindowSmall();
+
+window.onresize = function(){
+    location.reload();
+}
